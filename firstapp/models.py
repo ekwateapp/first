f891dd0dcaaf3fcc5eb2e6c0ccfece912a7bddbd
ekwateapp <ekwateapp@gmail.com>
2015-12-28 03:58:14 +0000

    added models

--- firstapp/models.py
+++ firstapp/models.py
@@ -5,3 +5,9 @@ from firstapp import db
 class TimeStamp(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     current_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    
+class Stuff(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(64))
+    time_stamp_id = db.Column(db.Integer, db.ForeignKey('time_stamp.id'))
+    timestamp = db.relationship(TimeStamp, backref=db.backref('stuff', lazy='dynamic'))
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,13 @@
+import random
+import string
+
 from flask import render_template
 
 from firstapp import app, db
-from firstapp.models import TimeStamp
+from firstapp.models import TimeStamp, Stuff
+
+def generate_string():
+    return ''.join([random.choice(string.ascii_uppercase) for _ in range(8)])
 
 @app.route('/')
 def index():
@@ -12,6 +18,19 @@ def timestamp():
     ts = TimeStamp()
     
     db.session.add(ts)
+    
+    stuff = Stuff(content=generate_string(), timestamp=ts)
+    
+    db.session.add(stuff)
+    
     db.session.commit()
     
-    return '{}'.format(ts.id)
+    return '{}'.format(stuff.id)
+    
+@app.route('/stuff/<int:id>')
+def get_stuff(id):
+    stuff = Stuff.query.get(id)
+    if stuff is None:
+        return 'no stuff found'
+    else:
+        return '{}, {}'.format(stuff.content, stuff.timestamp.current_time)
